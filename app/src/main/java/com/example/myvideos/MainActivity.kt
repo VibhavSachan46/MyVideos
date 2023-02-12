3d87b09583081ccb0ce45fcc585336930430fa2a
Vibhav <vibhavsachan743@gmail.com>
2023-02-12 11:28:56 +0530

    Permission , Navigation Drawer

--- app/src/main/java/com/example/myvideos/MainActivity.kt
+++ app/src/main/java/com/example/myvideos/MainActivity.kt
@@ -1,18 +1,32 @@
 package com.example.myvideos
 
+import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import com.example.myvideos.databinding.ActivityMainBinding
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+    lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        requestRunTime()
+        // for nav Drawer
+        toggle = ActionBarDrawerToggle(this, binding.root,R.string.open, R.string.close)
+        binding.root.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         setFragment(VideosFragment())
         binding.bottomnav.setOnItemSelectedListener {
             when(it.itemId)
@@ -22,11 +36,58 @@ class MainActivity : AppCompatActivity() {
             }
             return@setOnItemSelectedListener true
         }
+        binding.navView.setNavigationItemSelectedListener {
+            when(it.itemId)
+            {
+                R.id.feedbackNav -> Toast.makeText(this, "Feedback",Toast.LENGTH_SHORT).show()
+                R.id.themesNav -> Toast.makeText(this, "Themes",Toast.LENGTH_SHORT).show()
+                R.id.sortorder -> Toast.makeText(this, "sort oder",Toast.LENGTH_SHORT).show()
+                R.id.aboutNav -> Toast.makeText(this, "about",Toast.LENGTH_SHORT).show()
+                R.id.exit -> exitProcess(10)
+            }
+            return@setNavigationItemSelectedListener true
+        }
     }
     private fun setFragment(fragment: Fragment){
         val transaction = supportFragmentManager.beginTransaction()
         transaction.replace(R.id.fragmentFL, fragment)
         transaction.disallowAddToBackStack()
         transaction.commit()
+    }
+
+    //for requesting permission
+
+    private fun requestRunTime(): Boolean{
+        if(ActivityCompat.checkSelfPermission(this,WRITE_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this, arrayOf(WRITE_EXTERNAL_STORAGE),10)
+            return false
+        }
+        return true
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == 10)
+        {
+            if(grantResults[0]==PackageManager.PERMISSION_GRANTED)
+            {
+                Toast.makeText(this,"Permission Granted", Toast.LENGTH_SHORT).show()
+            }
+            else
+            {
+                requestRunTime()
+            }
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item))
+            return true
+        return super.onOptionsItemSelected(item)
     }
 }
