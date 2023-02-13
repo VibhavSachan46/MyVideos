f8e9e311b1e072697e666fb31f2b12c48dcd8735
Vibhav <vibhavsachan743@gmail.com>
2023-02-14 01:01:03 +0530

    Revert "Recycler View & Adapter"
    This reverts commit 4885fc2795c19bacba4ebd73e7596726b3ca840c.

--- app/src/main/java/com/example/myvideos/VideosFragment.kt
+++ app/src/main/java/com/example/myvideos/VideosFragment.kt
@@ -5,8 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.myvideos.databinding.FragmentVideosBinding
 
 class VideosFragment : Fragment() {
 
@@ -16,19 +14,7 @@ class VideosFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_videos, container, false)
-        val binding = FragmentVideosBinding.bind(view)
-        val tempList = ArrayList<String>()
-        tempList.add("First Video")
-        tempList.add("Second Video")
-        tempList.add("Third Video")
-        tempList.add("Fourth Video")
-        tempList.add("Fifth Video")
-        binding.VideoRV.setHasFixedSize(true)
-        binding.VideoRV.setItemViewCacheSize(10)
-        binding.VideoRV.layoutManager = LinearLayoutManager(requireContext())
-        binding.VideoRV.adapter = VideoAdapter(requireContext(), tempList)
-        return view
+        return inflater.inflate(R.layout.fragment_videos, container, false)
     }
 
 
